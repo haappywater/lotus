ff5ffa0fcc783fc653a09ef1742b738b27d80e03
Jakub Sztandera <kubuxu@protocol.ai>
2020-09-10 12:22:49 +0200

    Remove PackingEfficiency consideration from base fee in an upgrade
    Signed-off-by: Jakub Sztandera <kubuxu@protocol.ai>

--- build/params_testground.go
+++ build/params_testground.go
@@ -70,6 +70,8 @@ var (
 	PackingEfficiencyNum   int64 = 4
 	PackingEfficiencyDenom int64 = 5
 
-	UpgradeBreezeHeight      abi.ChainEpoch = 0
+	UpgradeBreezeHeight      abi.ChainEpoch = -1
 	BreezeGasTampingDuration abi.ChainEpoch = 0
+
+	UpgradeSmokeHeight abi.ChainEpoch = -1
 )
